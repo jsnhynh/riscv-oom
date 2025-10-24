2565085cfe81ece8ce84eb818680caee335c634b
Jason Huynh <47314658+jsnhynh@users.noreply.github.com>
2025-10-23 23:36:51 -0700

    Reimped rename, rob_entry write moved to dispatch

--- rtl/3-rename/rename.sv
+++ rtl/3-rename/rename.sv
@@ -1,9 +1,9 @@
 /*
     Rename Stage
 
-    This module renames registers, allocates ROB entries, and writes
-    initial metadata to ROB. It reads the PRF for operand status and 
-    writes new tag mappings back to the PRF's RAT
+    This module renames registers and allocates ROB entries. It reads 
+    the PRF for operand status and writes new tag mappings back to the
+    PRF's RAT. Actual ROB write is handled by the dispatch stage.
 */
 import uarch_pkg::*;
 
@@ -19,12 +19,9 @@ module rename (
     output logic            rename_val,
     input  logic            dispatch_rdy,
 
-    // Ports to/from ROB
-    // -- ROB entry Ports
-    input  logic [1:0]      rob_rdy,    // 00: 0 free, 01: 1 free, 10/11: 2+ free
-    output rob_entry_t      rob_entry0,     rob_entry1,
-    output logic [1:0]      rob_we,
-    // -- ROB prf Ports
+    // Ports from ROB
+    output logic [1:0]      rob_alloc_req,
+    input  logic [1:0]      rob_alloc_gnt, // Grant for 0, 1, or 2 entries
     input  logic [TAG_WIDTH-1:0]    rob_tag0,               rob_tag1,
     input  commit_write_port_t      commit_0_write_port,    commit_1_write_port
 );
@@ -34,7 +31,6 @@ module rename (
     prf_read_port_t rs1_0_read_port, rs2_0_read_port;
     prf_read_port_t rs2_1_read_port, rs1_1_read_port;
     prf_rat_write_port_t rat_0_write_port, rat_1_write_port;
-
 
     prf prf_inst (
         .clk(clk),
@@ -60,11 +56,11 @@ module rename (
         input prf_read_port_t rs1_port,
         input prf_read_port_t rs2_port,
         input logic [TAG_WIDTH-1:0] new_tag,
-        input logic                 can_rename_flag
+        input logic                 alloc_gnt
     );
         renamed_inst_t r_inst;
         r_inst = `{default:`0};
-        r_inst.is_valid = can_rename_flag;
+        r_inst.is_valid = alloc_gnt;
 
         // Pass-through fields from decode
         r_inst.pc           = d_inst.pc;
@@ -98,73 +94,54 @@ module rename (
     // Conbinational Renaming Logic
     //-------------------------------------------------------------
     renamed_inst_t renamed_inst0_next, renamed_inst1_next;
-    logic can_rename0, can_rename1;
 
-    int rob_slots_needed = decode_inst0.is_valid + decode_inst1.is_valid;
-    logic enough_rob_slots = rob_slots_needed <= rob_rdy;
-
-    // An instruction can be successfully renamed if it's valid and ROB has space for it
-    assign can_rename0 = decode_inst0.is_valid && enough_rob_slots;
-    assign can_rename1 = decode_inst1.is_valid && enough_rob_slots;
+    // Request ROB entries based on validity of incoming instructions
+    assign rob_alloc_req[0] = decode_inst0.is_valid;
+    assign rob_alloc_req[1] = decode_inst1.is_valid;
 
     always_comb begin
         renamed_inst_t renamed_inst0_tmp,  renamed_inst1_tmp;
 
         // Step 1: Perform initial renaming for both instructions
-        renamed_inst0_tmp = rename_inst(decode_inst0, rs1_0_read_port, rs2_0_read_port, rob_tag0, can_rename0);
-        renamed_inst1_tmp = rename_inst(decode_inst1, rs1_1_read_port, rs2_1_read_port, rob_tag1, can_rename1);
+        renamed_inst0_tmp = rename_inst(decode_inst0, rs1_0_read_port, rs2_0_read_port, rob_tag0, rob_alloc_gnt[0]);
+        renamed_inst1_tmp = rename_inst(decode_inst1, rs1_1_read_port, rs2_1_read_port, rob_tag1, rob_alloc_gnt[1]);
 
-        // Apply intra-group forwards logic for inst 1
-        if (can_rename0 && decode_inst0.has_rd && (decode_inst1.rs1 == decode_inst0.rd) && (decode_inst0.rd != 0)) begin //  inst0's rd == inst1's rs1?
+        // Step 2: Apply intra-group forwards logic for inst 1
+        if (rob_alloc_gnt[0] && decode_inst0.has_rd && (decode_inst1.rs1 == decode_inst0.rd) && (decode_inst0.rd != 0)) begin //  inst0's rd == inst1's rs1?
             renamed_inst1_tmp.rs1_renamed   = 1'b1;
             renamed_inst1_tmp.rs1_tag       = rob_tag0;
         end
 
-        if (can_rename0 && decode_inst0.has_rd && (decode_inst1.rs2 == decode_inst0.rd) && (decode_inst0.rd != 0)) begin //  inst0's rd == inst1's rs2?
+        if (rob_alloc_gnt[0] && decode_inst0.has_rd && (decode_inst1.rs2 == decode_inst0.rd) && (decode_inst0.rd != 0)) begin //  inst0's rd == inst1's rs2?
             renamed_inst1_tmp.rs2_renamed   = 1'b1;
             renamed_inst1_tmp.rs2_tag       = rob_tag0;
         end
 
-        renamed_inst0_next = renamed_inst0_tmp;
-        renamed_inst1_next = renamed_inst1_tmp;
+        // Step 3: Compaction Logic
+        if (!renamed_inst0.is_valid && renamed_inst1.is_valid) begin
+            renamed_inst0_next = renamed_inst1_tmp;
+            renamed_inst1_next = '{default:'0};
+        end else begin
+            renamed_inst0_next = renamed_inst0_tmp;
+            renamed_inst1_next = renamed_inst1_tmp;
+        end
     end
 
     // -- Generate Write Ports for PRF --
     assign rat_0_write_port.addr = decode_inst0.rd;
     assign rat_0_write_port.tag  = rob_tag0;
-    assign rat_0_write_port.we   = can_rename0 && decode_inst0.has_rd;
+    assign rat_0_write_port.we   = rob_alloc_gnt[0] && decode_inst0.has_rd;
 
     assign rat_1_write_port.addr = decode_inst1.rd;
     assign rat_1_write_port.tag  = rob_tag1;
-    assign rat_1_write_port.we   = can_rename1 && decode_inst1.has_rd;
-
-    //-------------------------------------------------------------
-    // ROB Entry Generation
-    //-------------------------------------------------------------
-    function automatic rob_entry_t gen_rob_entry(input decoded_inst_t d_inst);
-        rob_entry_t entry;
-        entry.is_valid  = 1'b1;
-        entry.is_ready  = 1'b0;
-        entry.pc        = d_inst.pc;
-        entry.rd        = d_inst.rd;
-        entry.has_rd    = d_inst.has_rd;
-        entry.result    = '0;
-        entry.has_exception = 1'b0;
-        entry.is_branch = d_inst.is_branch;
-        entry.is_jump   = d_inst.is_jump;
-        entry.is_store  = d_inst.is_store;
-        return entry;
-    endfunction
-
-    assign rob_we = {can_rename1, can_rename0};
-    assign rob_entry0 = gen_rob_entry(decode_inst0);
-    assign rob_entry1 = gen_rob_entry(decode_inst1);
+    assign rat_1_write_port.we   = rob_alloc_gnt[1] && decode_inst1.has_rd;
 
     //-------------------------------------------------------------
     // Handshake and Pipeline Control
     //-------------------------------------------------------------
-    assign rename_rdy = dispatch_rdy && ~cache_stall && enough_rob_slots;
-    assign rename_val = can_rename0 || can_rename1;
+    logic can_advance = ~decode_inst0.is_valid || (decode_inst0.is_valid && rob_alloc_gnt[0]);
+    assign rename_rdy = dispatch_rdy && ~cache_stall && can_advance;
+    assign rename_val = rob_alloc_gnt[0] || rob_alloc_gnt[1];
 
     //-------------------------------------------------------------
     // Pipeline Register Logic
